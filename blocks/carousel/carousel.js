ba234a5a23d3935231442f1dc527dbb322df7cb1
Karthick <152947321+Karthick2704@users.noreply.github.com>
2024-06-17 23:54:02 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -2,33 +2,35 @@ import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
 const placeholders = await fetchPlaceholders(getMetadata("locale"));
 
 const { btnNxt,btnPre} = placeholders;
-export default function decorate (block) {
-const rows = [...block.children];
-[...block.children].forEach((row,r) => {
-if(r==0){
-  const nextbtn = document.createElement('button');
-	nextbtn.classList.add('btn');
-	nextbtn.classList.add('btn-next');
-	const node = document.createTextNode(row.textContent);
-	nextbtn.append(node);
-	row. replaceWith(nextbtn);
-	}else if (r= rows.length-1){
-	const prebtn = document.createElement('button');
-	prebtn.classList.add('btn');
-	prebtn.classList.add('btn-prev');
-			const node = document.createTextNode(row. textContent);
+export default function decorate(block) {
+
+  console.log("placeholders ---> ",placeholders,btnNxt,btnPre);
+    const rows= [...block.children];
+    [...block.children].forEach((row,r) => {
+        if (r == 0) {
+			const nextbtn = document.createElement('button');
+			nextbtn.classList.add('btn');
+			nextbtn.classList.add('btn-next');
+			const node = document.createTextNode(row.textContent);
+			nextbtn.append(node);
+			row.replaceWith(nextbtn);
+		} else if (r = rows.length - 1) {
+			const prebtn = document.createElement('button');
+			prebtn.classList.add('btn');
+			prebtn.classList.add('btn-prev');
+			const node = document.createTextNode(row.textContent);
 			prebtn.append(node);
 			row.replaceWith(prebtn);
-			}else{
+		} else {
 			row.classList.add('slide');
 			[...row.children].forEach((col, c) => {
-			console.log("====>", row, r, col, c);
-			if (c==1) {
-			col.classList.add('slide-text');
-			}
+				console.log("====>", row, r, col, c);
+				if (c == 1) {
+					col.classList.add('slide-text');
+				}
 			});
-			}
-			});
+		}
+    }); 
   
   
     const slides = document.querySelectorAll(".slide");
