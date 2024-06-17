604fc103abf19a5ac126a6a4d12c2f028a7a2566
Karthick <152947321+Karthick2704@users.noreply.github.com>
2024-06-17 23:58:41 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -14,22 +14,23 @@ export default function decorate(block) {
 			const node = document.createTextNode(row.textContent);
 			nextbtn.append(node);
 			row.replaceWith(nextbtn);
-		} else if (r = rows.length - 1) {
+		}
+        else if (r = rows.length - 1) {
 			const prebtn = document.createElement('button');
 			prebtn.classList.add('btn');
 			prebtn.classList.add('btn-prev');
 			const node = document.createTextNode(row.textContent);
 			prebtn.append(node);
 			row.replaceWith(prebtn);
-		} else {
-			row.classList.add('slide');
-			[...row.children].forEach((col, c) => {
-				console.log("====>", row, r, col, c);
-				if (c == 1) {
-					col.classList.add('slide-text');
-				}
-			});
-		}
+		}else
+            {
+                row.classList.add('slide');
+                [...row.children].forEach((col,c) => {
+                    if(c==1){
+                        col.classList.add('slide-text');
+                    }        
+                });
+            }
     }); 
   
   
